f10dce924696a24b6a6fc7fd1b951b5eccecc4c2
aanchal <waliaaanchal02@gmail.com>
2022-01-30 18:57:07 +0530

    completed

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ window.onload = async() =>{
     generateScales()
     //drawbars()
     generateAxis()
+    drawbars()
     
 } 
 
@@ -79,18 +80,66 @@ let generateScales = () => { //it  is used to generate our scales
    
 let generateAxis = () => {
 
-            let xAxis = d3.axisBottom(xAxisScale)
+            let xAxis = d3.axisBottom(xAxisScale)//function to create the x axis
+            let yAxis = d3.axisLeft(yAxisScale)//function to create the y axis
         
             svg.append('g')
                 .call(xAxis)
                 .attr('id', 'x-axis')
                 .attr('transform', 'translate(0, ' + (height - padding) + ')')  // this here is used to move the scale with the help of coordinates.
+
+            svg.append('g')
+                .call(yAxis)
+                .attr("id", "y-axis")
+                .attr("transform", ' translate( ' + (padding) + ' ,0) ')
+            
         }
     
    
-let drawbars = () => {
+let drawbars =() => {
 
-}
+            let tooltip = d3.select('body')
+                            .append('div')
+                            .attr('id', 'tooltip')
+                            .style('visibility', 'hidden')
+                            .style('width', 'auto')
+                            .style('height', 'auto')
+        
+            svg.selectAll('rect')
+                .data(values)
+                .enter()
+                .append('rect')
+                .attr('class', 'bar')
+                .attr('width', (width - (2 * padding)) / values.length)
+                .attr('data-date', (item) => {
+                    return item[0]
+                })
+                .attr('data-gdp', (item) => {
+                    return item[1]
+                })
+                .attr('height', (item) => {
+                    return heightScale(item[1])
+                })
+                .attr('x', (item, index) => {
+                    return xScale(index)
+                })
+                .attr('y', (item) => {
+                    return (height - padding) - heightScale(item[1])
+                })
+                .on('mouseover', (event, item) => {
+                    tooltip.transition()
+                        .style('visibility', 'visible')
+        
+                    tooltip.text(item[0])
+        
+                    document.querySelector('#tooltip').setAttribute('data-date', item[0])
+                })
+                .on('mouseout', (item) => {
+                    tooltip.transition()
+                        .style('visibility', 'hidden')
+                })        
+        }
+        
 // to draw our bars
 
 
